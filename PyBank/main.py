297cf6dbd663d259b1a4594811185b29edec70f2
Carl Colburn <carlcolburn@gmail.com>
2023-07-15 12:08:02 -0700

    updated path to csv files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,15 @@
+import os
+import csv
 
+csvpath = os.path.join('..', 'Resources','Starter_Code', 'PyBank', 'Resources','budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        print(row)
