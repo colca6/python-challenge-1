9abdceb62f24ed93024893ed2b09ce5663005da6
Carl Colburn <carlcolburn@gmail.com>
2023-07-18 20:35:27 -0700

    Commit homework 3 working files 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,20 @@
+#set up environment
 import os
 import csv
 
-csvpath = os.path.join('PyBank','Resources','budget_data.csv')
-#csvpath = os.path.join('PyPoll','Resources','election_data.csv')
+#create relative path to file to read
+csvpath = os.path.join('Resources','budget_data.csv')
 
-with open(csvpath) as csvfile:    #, #newline=""
-
+#open file with reader object
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    #save header info here:
     csv_header = next(csvreader)
+    #save value in the first row so when we iterate, we have a starting value to compare new row data to.
     firstrow = next(csvreader)
+
     #Lots of variables to create to store values
-    total_months = 1
+    total_months = 0
     previous_profit = int(firstrow[1])
     total_change = 0
     greatest_increase = int(firstrow[1])
@@ -19,6 +22,7 @@ with open(csvpath) as csvfile:    #, #newline=""
     months = 0
     profit = int(firstrow[1])
     
+    #loop through data
     for row in csvreader:
         #get total numbr of months
         total_months += 1
@@ -33,53 +37,37 @@ with open(csvpath) as csvfile:    #, #newline=""
         total_change += difference
         average_change = total_change/(total_months -1)
 
+        #Double whammy-do math to check for numerical increase/decrease while also holding the associated month
         if difference > greatest_increase:
             greatest_increase = difference
             month_greatest_increase = row[0]
         elif difference < greatest_decrease:
             greatest_decrease = difference
             month_greatest_decrease = row[0]
-
+        #reset "previous_profit" to that row's data so we can compare new month to it
         previous_profit = int(row[1])
 
-    #average_change = total_change/total_months   
-        #month.count(row[0])
-        #profit.append(row[1])
-        #profit.sort(reverse=True)
-
-    #months = month.count(row[0])
+    #Print to terminal
     print("Financial Analysis")
     print("-----------------------------")
     print("Total Months : " + str(total_months))
     print("Total : " + str(profit))
-    print("Average Change : " + str(average_change))
+    print("Average Change : " + str(round(average_change,2)))    #round to two decimal points
     print("Greatest Increase in Profits : " + str(month_greatest_increase + " (" + str(greatest_increase) + ")" ))
     print("Greatest Decrease in Profits : " + str(month_greatest_decrease + " (" + str(greatest_decrease) + ")" ))
 
+    #Write out to text file instructions
+    #Note position of indentation.
+outpath = os.path.join('Analysis','analysis_results.txt')
 
-
-
-
-
-"""     print(difference)
-    print(total_change)
-    print(greatest_increase)
-    print(greatest_decrease)
-    print(month_greatest_increase)
-    print(month_greatest_decrease)
-    print(greatest_decrease) """
-       
-    
-    #print(f"CSV Header: {csv_header}")
-    #print(csvreader)
-
-    #for row in csvreader:
-    #    print(row)
-        
-        #Task 1 of homework, number of months
-
-    
-    #netTotal = 0
-    #for row in csvreader:
-    #    netTotal += float(row[1])
-    #print(netTotal)    
+#open text file to write into. Call .write().    '\n' makes line break, else would all be one line.
+#Also 'w' inside () indicates write to it.
+with open(outpath, 'w') as file:
+    file.write("Financial Analysis" + '\n')
+    file.write("-----------------------------" + '\n')
+    file.write("Total Months : " + str(total_months) + '\n')
+    file.write("Total : " + str(profit) + '\n')
+    file.write("Average Change : " + str(round(average_change,2)) + '\n')
+    file.write("Greatest Increase in Profits : " + str(month_greatest_increase + " (" + str(greatest_increase) + ")" ) + '\n')
+    file.write("Greatest Decrease in Profits : " + str(month_greatest_decrease + " (" + str(greatest_decrease) + ")" ))
+ 
